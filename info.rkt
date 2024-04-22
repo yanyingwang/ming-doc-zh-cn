81042c76da2c26e78ae2b1fa3c83aeabe479f89b
Yanying Wang <yanyingwang1@gmail.com>
2024-04-22 23:15:03 +0800

    fix build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "ming-doc-zh-cn")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "https://github.com/yanyingwang/ming.git#0.0.3" "scribble-rainbow-delimiters"))
+(define deps '("base" "scribble-lib" "at-exp-lib"))
+(define build-deps '("sandbox-lib" "scribble-doc" "scribble-lib" "racket-doc" "rackunit-lib" "scribble-rainbow-delimiters" "https://github.com/yanyingwang/ming.git#0.0.3"))
 (define scribblings '(("scribblings/ming-doc-zh-cn.scrbl" ())))
 (define pkg-desc "the zh cn version doc of ming lang")
 (define version "0.1")
